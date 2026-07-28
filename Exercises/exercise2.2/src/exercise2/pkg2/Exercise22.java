4d3e14754941ae091332a5b3513d49ebd181893a
Thato-Motseki <thatomotseki292@gmail.com>
2026-07-28 10:12:07 +0200

    First 10 Natural numbers

---
+++ Exercises/exercise2.2/src/exercise2/pkg2/Exercise22.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise2.pkg2;
+
+/**
+ *
+ * @author thato
+ */
+public class Exercise22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /**Program to output the first 10 natural numbers**/
+        
+        for (int i = 1; i <= 10; i++){
+            System.out.print( i + ",");
+        }
+    }
+    
+}
