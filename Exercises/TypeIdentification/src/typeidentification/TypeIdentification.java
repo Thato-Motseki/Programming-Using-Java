7c8027c2d418108f8b23df21ba478bce0dc7339d
Thato-Motseki <thatomotseki292@gmail.com>
2026-07-30 15:49:28 +0200

    Type Identification

---
+++ Exercises/TypeIdentification/src/typeidentification/TypeIdentification.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package typeidentification;
+
+/**
+ *
+ * @author thato
+ */
+public class TypeIdentification {
+
+    public static void main(String[] args) {
+        // Declare, assign and identify types
+        String value1 = "90210";
+        double value2 = 42.0;
+        boolean value3 = true;
+        char value4 = '-'; 
+        int value5 = 0;
+
+        // Print values and their types
+        System.out.println("Value\t\tData Type");
+        System.out.println("--------------------------");
+        System.out.println(value1 + "\t\tString");
+        System.out.println(value2 + "\t\tdouble");
+        System.out.println(value3 + "\t\tboolean");
+        System.out.println(value4 + "15\t\tchar / String");
+        System.out.println(value5 + "\t\tint");
+    }
+}
