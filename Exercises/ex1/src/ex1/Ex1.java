a17c928f734d5b1e0ca924fbf1c13d8c119e057c
Thato-Motseki <thatomotseki292@gmail.com>
2026-07-31 07:49:22 +0200

    Add program to concatenate two strings with space

---
+++ Exercises/ex1/src/ex1/Ex1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex1;
+
+/**
+ *
+ * @author thato
+ */
+public class Ex1 {
+    public static void main(String[] args) {
+        // Declare and initialize two strings
+        String firstName = "Thato";
+        String surname = "Motseki";
+        
+        // Concatenate with space and store in third variable
+        String fullName = firstName + " " + surname;
+        
+        // Print result
+        System.out.println(fullName);
+    }
+}
