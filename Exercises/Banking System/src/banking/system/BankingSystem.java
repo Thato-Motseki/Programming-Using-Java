0be29056436080ca6349d4f7d48abc97ecaee7f7
Thato-Motseki <thatomotseki292@gmail.com>
2026-08-06 21:55:53 +0200

    Program Add: A mini banking system

---
+++ Exercises/Banking System/src/banking/system/BankingSystem.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package banking.system;
+import java.util.Scanner;
+
+/**
+ *
+ * @author thato
+ */
+public class BankingSystem {
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner (System.in);
+        
+        final String Stored_Username = "Admin_User";
+        final String Stored_Password = "Pass@123";
+        
+        
+        System.out.print("Enter your Username: ");
+        String username = input.next();
+        
+        System.out.print("Enter your password: ");
+        String password = input.next();
+        
+        boolean isAuthenticated = username.equals(Stored_Username) && password.equals(Stored_Password);
+        
+        if (isAuthenticated){
+            System.out.println("Access granted!");
+        }
+        else {
+            System.out.println("Access denied!");
+        }
+        
+    }    
+}
