a438d8ae5d6b36e080bf7c062c456b1fe43073b6
Thato-Motseki <thatomotseki292@gmail.com>
2026-08-24 22:18:17 +0200

    staged commit

--- Exercises/class2/src/class2/BankAccount.java
+++ Exercises/class2/src/class2/BankAccount.java
@@ -4,17 +4,50 @@
  */
 package class2;
 
-/**
- *
- * @author thato
- */
-public class Class2 {
+public class BankAccount {
+    private String accountHolder;
+    private double balance;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    // Constructor
+    public BankAccount(String accountHolder, double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
     }
-    
+
+    // Deposit money
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: M" + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    // Withdraw money
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: M" + amount);
+        } else {
+            System.out.println("Insufficient funds or invalid amount.");
+        }
+    }
+
+    // Display account information
+    public void displayAccount() {
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Balance: M" + balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("Tay-Tay", 5000);
+
+        account.displayAccount();
+
+        account.deposit(1500);
+        account.withdraw(2000);
+
+        account.displayAccount();
+    }
 }
