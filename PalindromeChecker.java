153069baecc1cd4f8f0c9c14245278c0525ef786
Thato Motseki <thatomotseki292@gmail.com>
2026-09-16 23:17:25 +0200

    Add palindrome checker program

---
+++ PalindromeChecker.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
